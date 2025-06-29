4a14970b73ec593bf2292bf2d82bd38f915e7334
Stage Devices <developer@stagedevices.com>
2025-06-29 03:17:39 -0700

    Create Contacts.tsx

---
+++ web/src/components/Contacts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+
+export function Contacts() {
+  return (
+    <Card title="Contacts" style={{ margin: '2rem', borderRadius: '1.5rem' }}>
+      {/* TODO: list of contacts */}
+    </Card>
+  );
+}
