1e336b2eef7c3a98d7b8af67d229f85f3a0daf22
Stage Devices <developer@stagedevices.com>
2025-06-29 14:42:02 -0700

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import fetch from "node-fetch";
+import { fetch } from "undici"
 import cors from "cors";
 import { FieldValue } from "firebase-admin/firestore";
 
