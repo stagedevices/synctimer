d0f8adb0eee15fed2494977caf6d111bcd0d71c8
Stage Devices <developer@stagedevices.com>
2025-06-29 03:18:33 -0700

    Create LinkPhone.tsx

---
+++ web/src/components/LinkPhone.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+
+export function LinkPhone() {
+  return (
+    <Card title="Link Phone" style={{ margin: '2rem', borderRadius: '1.5rem' }}>
+      {/* TODO: list of uploaded/parsed YAML files */}
+    </Card>
+  );
+}
