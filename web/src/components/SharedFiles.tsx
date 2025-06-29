a8b30dafb460a722306744214281f557c630631f
Stage Devices <developer@stagedevices.com>
2025-06-29 03:15:25 -0700

    Create SharedFiles.tsx

---
+++ web/src/components/SharedFiles.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+
+export function SharedFiles() {
+  return (
+    <Card title="Shared Files" style={{ margin: '2rem', borderRadius: '1.5rem' }}>
+      {/* TODO: list of shared YAML files */}
+    </Card>
+  );
+}
