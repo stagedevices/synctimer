c18077d839006f69c6cda05f75a847ba5bcb1db2
Stage Devices <developer@stagedevices.com>
2025-06-29 03:16:47 -0700

    Create SentFiles.tsx

---
+++ web/src/components/SentFiles.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+
+export function SentFiles() {
+  return (
+    <Card title="Sent Files" style={{ margin: '2rem', borderRadius: '1.5rem' }}>
+      {/* TODO: list of sent YAML files */}
+    </Card>
+  );
+}
