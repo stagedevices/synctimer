391d7a2365896f0cb0d4d44bc037c08d1d94da36
Stage Devices <developer@stagedevices.com>
2025-06-29 03:21:28 -0700

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,15 @@
 // web/src/App.tsx
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { UploadValidate } from "./components/UploadValidate";
+import { Home } from './components/Home';
+import { UploadValidate } from './components/UploadValidate';
+import { MyFiles } from './components/MyFiles';
+import { SharedFiles } from './components/SharedFiles';
+import { SentFiles } from './components/SentFiles';
+import { Contacts } from './components/Contacts';
+import { LinkPhone } from './components/LinkPhone';
+import { Settings } from './components/Settings';
+
 
 export function App() {
   return (
