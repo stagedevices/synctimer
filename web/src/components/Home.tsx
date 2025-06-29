0ea59f5094fdb7fec687fe253f9b63acfdc980ff
Stage Devices <developer@stagedevices.com>
2025-06-29 03:22:35 -0700

    Create Home.tsx

---
+++ web/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+
+export function Home() {
+  return (
+    <Card title="Home" style={{ margin: '2rem', borderRadius: '1.5rem' }}>
+      {/* TODO: list of uploaded/parsed YAML files */}
+    </Card>
+  );
+}
