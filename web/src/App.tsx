e324982e720398697f5a22cdd57548d2062898eb
Stage Devices <developer@stagedevices.com>
2025-06-29 20:37:06 -0700

    Implement OAuth landing page and route guard (#18)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,21 @@
 // web/src/App.tsx
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
-import { Home } from './components/Home';
 import { UploadValidate } from './components/UploadValidate';
 import { MyFiles } from './components/MyFiles';
 import SharedFiles from './components/SharedFiles';
 import { SentFiles } from './components/SentFiles';
 import { Contacts } from './components/Contacts';
 import { Devices } from './components/Devices';
-import { Account } from './components/Account';
+import { Account } from './pages/Account';
 import Settings from './components/Settings';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './lib/firebase';
-import { PrivateRoute } from './components/PrivateRoute';
+import { Spin } from 'antd';
 
 export function App() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  if (loading) return <Spin />;
+
   return (
     <BrowserRouter>
       {user && (
@@ -30,72 +31,22 @@ export function App() {
         </nav>
       )}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/account"
-          element={
-            <PrivateRoute>
-              <Account />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <PrivateRoute>
-              <Settings />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/parse"
-          element={
-            <PrivateRoute>
-              <UploadValidate />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/files"
-          element={
-            <PrivateRoute>
-              <MyFiles />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/shared"
-          element={
-            <PrivateRoute>
-              <SharedFiles />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/sent"
-          element={
-            <PrivateRoute>
-              <SentFiles />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute>
-              <Contacts />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/devices"
-          element={
-            <PrivateRoute>
-              <Devices />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/account" element={<Account />} />
+        {!auth.currentUser ? (
+          <Route path="*" element={<Navigate to="/account" replace />} />
+        ) : (
+          <>
+            <Route path="/" element={<Navigate to="/parse" replace />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/parse" element={<UploadValidate />} />
+            <Route path="/files" element={<MyFiles />} />
+            <Route path="/shared" element={<SharedFiles />} />
+            <Route path="/sent" element={<SentFiles />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="*" element={<Navigate to="/parse" replace />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
