1e40231cabf1102dc98747fd072e9e9d41e750a2
Stage Devices <developer@stagedevices.com>
2025-06-29 18:03:49 -0700

    fix build errors

--- web/src/components/Contacts.tsx
+++ web/src/components/Contacts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { List, Card, Button } from "antd";
 import { db, auth } from "../lib/firebase";
 import {
